31df015f49e0c62bff61863fad781529937a53a5
Aman Mahajan <ammahajan@expedia.com>
2019-06-21 22:51:44 +0530

    added comments

--- src/main/scala/com/scala/practice/movieratings/MovieRatingsHistogram.scala
+++ src/main/scala/com/scala/practice/movieratings/MovieRatingsHistogram.scala
@@ -2,7 +2,7 @@ package com.scala.practice.movieratings
 
 import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.SparkContext
-
+import org.apache.spark.rdd.RDD
 
 object MovieRatingsHistogram {
 
@@ -10,14 +10,23 @@ object MovieRatingsHistogram {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
     val logger = LogManager.getLogger(this.getClass.getName)
-    logger.info("Hello World "+SparkContext.getClass.toString)
+    logger.info("Hello World " + SparkContext.getClass.toString)
 
-
+    /*
+      the * here suggests that we can use all of the cores inside our system to obtain parallelism
+      The second argument would be the application name
+     */
     val sc = new SparkContext("local[*]", "MovieRatingsCounter")
 
-    val line = sc.textFile("/Users/ammahajan/Personal/Spark-Practice/src/main/resources/ml-100k/u.data")
-    logger.info(line.count())
 
+    val line: RDD[String] = sc.textFile("/Users/ammahajan/Personal/Spark-Practice/src/main/resources/ml-100k/u.data")
+
+    val ratings: RDD[String] = line.map(_.toString).map(_.split("\t")(2))
+    val count: collection.Map[String, Long] = ratings.countByValue()
+    /*
+    Print each value of the Map
+     */
+    println(count.toSeq.sortBy(_._1))
 
   }
 
